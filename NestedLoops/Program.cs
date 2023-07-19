f6b9e00e28e5f164ac88553ae6acb31dfa806bb4
elihl <elihl@LAPTOP-EVR5KTER>
2023-07-19 04:20:04 +0200

    Add project files.

---
+++ NestedLoops/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i=10; i>0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
